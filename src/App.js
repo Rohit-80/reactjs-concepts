bac8ad7136984b23a7ca61bd9b82cb35ec5f3647
Rohit-80 <shrirohitvishwakarma80@gmail.com>
2023-01-18 13:03:51 +0530

    First

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React ,{ useState } from "react";
+
+function App(){
+
+  // calling every time
+  // const [count,setcount] = useState(4);
+
+  // calling using fuction
+  // const [count,setcount] = useState(()=>{
+  //     console.log('run fuction')
+  //   return 4;
+  // });
+
+  // with objects
+
+  // const [state,setState] = useState({count : 4 , theme : 'blue'});
+  // const count = state.count
+  // const theme = state.theme
+
+  // with two hooks 
+  const [state,setState] = useState(4);
+  const [theme,setTheme] = useState('blue');
+
+  
+
+  // its not a good way to change the value 
+  // except it has another version with fucntion
+  function inc(){
+    // setcount(count+1)
+    // setState((preState) =>{
+    //  return  {...preState ,count : preState.count - 1}
+    // })
+    // setState(prevocunt=>prevocunt-1)
+    // setTheme('red')
+  }
+  function dec(){
+    setState((prevocunt) =>{
+      return prevocunt++;
+   })
+  }
+
+  return(
+    <> 
+     <button onClick={inc}>inc</button>
+      <span>{state}</span>
+      <span>{theme}</span>
+      <button onClick={dec}> dec</button>
+    </>
+  )
+}
+
+export default App;
